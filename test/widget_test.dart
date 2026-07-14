1a0149941dd3d413923101ea72f53c2e156348ac
Kalana-methsara <kalanamethsara53@gmail.com>
2026-07-14 07:44:10 +0530

    fix test file name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:flash_go/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const CampRunnerApp());
+    await tester.pumpWidget(const FlashGoApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
